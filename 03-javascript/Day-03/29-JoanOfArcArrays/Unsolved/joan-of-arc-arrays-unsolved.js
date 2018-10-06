4841e5ea579747d3fe9c292eacdf9ee05b225147
Requ Amer <requamer@calvisitor-10-105-191-166.calvisitor.1918.berkeley.edu>
2018-10-06 11:05:13 -0700

    update

---
+++ 03-javascript/Day-03/29-JoanOfArcArrays/Unsolved/joan-of-arc-arrays-unsolved.js
@@ -0,0 +1,26 @@
+// Joan of Arc "properties".
+var joanOfArcInfoParts = ["Real Name", "Grew Up Where", "Known For", "Scars", "Symbolism"];
+
+// Values for Joan's "properties".
+var joanOfArcInfoValues = [
+  "Jehanne la Pucelle.",
+  "Domremy, a village in northeastern France.",
+  "Peasant girl, daughter of a farmer, who rose to become Commander of the French army.",
+  "Took an arrow to the shoulder and a crossbow bolt to the thigh while trying to liberate Paris.",
+  "Stands for French unity and nationalism."
+];
+var input = "Real Name";
+var index = joanOfArcInfoParts.indexOf(input);
+var valuesIndex = joanOfArcInfoValues[index];
+
+
+
+var car = {
+  brand: "BMW",
+  color: "Pearl white",
+  numberOfTires: 4,
+  isStick: true,
+  numberOfDoors: 2,
+
+}
+car.brand = "subbarru";
